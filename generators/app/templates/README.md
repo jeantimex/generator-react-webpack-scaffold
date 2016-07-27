effef6f22633e6c454842250e0da51e363cde848
jeantimex <jean.timex@gmail.com>
2016-07-26 20:47:19 -0700

    Updated template files.

---
+++ generators/app/templates/README.md
@@ -0,0 +1,2 @@
+# React Webpack Boilerplate
+
---
+++ generators/app/templates/karma.config.js
@@ -0,0 +1,65 @@
+var webpackConfig = require('./webpack.config.karma');
+
+module.exports = function (config) {
+
+  config.set({
+
+    browsers: ['PhantomJS'],
+
+    captureConsole: true,
+
+    colors: true,
+
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        { type: 'html', subdir: 'html' },
+        { type: 'lcov', subdir: 'lcov' },
+      ],
+    },
+
+    files: [
+      './node_modules/phantomjs-polyfill/bind-polyfill.js',
+      'test/test.bundle.js',
+    ],
+
+    // frameworks to use
+    frameworks: ['mocha', 'chai', 'chai-sinon'],
+
+    plugins: [
+      'karma-chai',
+      'karma-chai-sinon',
+      'karma-coverage',
+      'karma-mocha',
+      'karma-mocha-reporter',
+      'karma-phantomjs-launcher',
+      'karma-sourcemap-loader',
+      'karma-spec-reporter',
+      'karma-threshold-reporter',
+      'karma-webpack',
+    ],
+
+    preprocessors: {
+      // only specify one entry point
+      // and require all tests in there
+      'test/test.bundle.js': ['webpack', 'sourcemap'],
+    },
+
+    reporters: ['mocha', 'coverage', 'threshold'],
+
+    thresholdReporter: {
+        statements: 90,
+        branches: 90,
+        functions: 90,
+        lines: 90,
+    },
+
+    webpack: webpackConfig,
+
+    webpackMiddleware: {
+      noInfo: true,
+    },
+
+  });
+
+};
